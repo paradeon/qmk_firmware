8f938410d0400d2d532c46af3ff3c51f89c8d2d1
Arthur Yu <paradeon@gmail.com>
2019-08-20 22:05:48 +0800

    add jd40 setup

--- keyboards/jd40/keymaps/paradeon/config.h
+++ keyboards/jd40/keymaps/paradeon/config.h
@@ -1,7 +1,9 @@
 #include "../../config.h"
 
 //#define TAPPING_TOGGLE 2
-#define TAPPING_TERM 200
+//#define TAPPING_TERM 200
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 2000
 #define PERMISSIVE_HOLD
 #define LEADER_TIMEOUT 300
 /* #define IGNORE_MOD_TAP_INTERRUPT */
